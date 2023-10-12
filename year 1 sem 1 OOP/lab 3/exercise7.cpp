53cfc31e108cd7c9808e778512604187edc1e29e
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-10-12 10:34:46 +0300

    Exercise 7 solution

--- year 1 sem 1 OOP/lab 3/exercise7.cpp
+++ year 1 sem 1 OOP/lab 3/exercise7.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int main()
 {
-    int firstNum, secondNum;
+    int firstNum;
+    int secondNum;
     string operation = "";
 
     cout << "Enter a value for the first number: ";
     cin >> firstNum;
     cout << "Enter a value for the second number: ";
-    cin >> firstNum;
+    cin >> secondNum;
     cout << "Enter an operator: ";
     cin >> operation;
+
+    cout << "firstNum: " << firstNum << endl;
+    cout << "secondNum: " << secondNum << endl;
 
     double sum = 0;
 
