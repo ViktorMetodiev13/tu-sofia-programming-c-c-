a86eea8926181526f6bfdf99096b8555e5864b85
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-10-19 10:38:05 +0300

    Exercise 5 solution

---
+++ year 1 sem 1 OOP/lab 4/exercise5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    int x;
+
+    cout << "Enter value for x: ";
+    cin >> x;
+
+    if (x < 0)
+    {
+        cout << "The value for x should be 0 or positive number. Enter other value for x: ";
+        cin >> x;
+    }
+
+    double result = sqrt(x);
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
