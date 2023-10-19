b6828ff0651ece603c28a24b13941b768395f399
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-10-19 09:33:57 +0300

    Improved solution for exercise 3

--- year 1 sem 1 OOP/lab 4/exercise3.cpp
+++ year 1 sem 1 OOP/lab 4/exercise3.cpp
@@ -10,14 +10,21 @@ int main()
     cout << "Enter value for b: ";
     cin >> b;
 
+    bool isDevisibleByThree = false;
+
     for (size_t i = a; i <= b; i++)
     {
         if (i % 3 == 0)
         {
+            isDevisibleByThree = true;
             cout << "i: " << i << endl;
         }
     }
 
+    if (isDevisibleByThree == false)
+    {
+        cout << "There are no such numbers" << endl;
+    }
 
     return 0;
 }
