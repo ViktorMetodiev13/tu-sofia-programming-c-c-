2b1715ac3a1bbddf8b60a8a5bafc018144cdd80e
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-10-19 11:06:59 +0300

    Exercise 5 improved solution

--- year 1 sem 1 OOP/lab 4/exercise5.cpp
+++ year 1 sem 1 OOP/lab 4/exercise5.cpp
@@ -10,17 +10,12 @@ int main()
     cout << "Enter value for x: ";
     cin >> x;
 
-    if (x < 0)
+    do
     {
         cout << "The value for x should be 0 or positive number. Enter other value for x: ";
         cin >> x;
-    }
-
-    if (x < 0)
-    {
-        cout << "The value for x should be 0 or positive number. Enter other value for x: ";
-        cin >> x;
-    }
+    } while (x < 0);
+    
 
     double result = sqrt(x);
 
