fbff4cfd2c7282729f413c2e6f9cf3c78f2000a1
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-10-19 11:05:13 +0300

    Exercise 6 improved solution

--- year 1 sem 1 OOP/lab 4/Exercise6.cpp
+++ year 1 sem 1 OOP/lab 4/Exercise6.cpp
@@ -12,11 +12,17 @@ int main()
     cout << "Enter value for degrees: ";
     cin >> degrees;
 
-    if (degrees < 45 || degrees > 60)
+    // if (degrees < 45 || degrees > 60)
+    // {
+    //     cout << "The value for degrees should be between 45 and 60 (included). Enter other value for degrees: ";
+    //     cin >> degrees;
+    // }
+
+    do
     {
-        cout << "The value for degrees should be between 45 and 60 (included). Enter other value for degrees: ";
+        cout << "Incorrect value, degrees should be between 45 and 60 (included). Try again: ";
         cin >> degrees;
-    }
+    } while (degrees < 45 || degrees > 60);
 
     double result = cos((degrees * M_PI) / 180);
 
