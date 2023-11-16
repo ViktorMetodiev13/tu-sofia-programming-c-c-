6dc3dce7ee79d8f38625c30dce2e8ec3251b7b5e
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-11-17 01:30:56 +0200

    Changes

--- year 1 sem 1 OOP/lab 4 - cycles/Exercise6.cpp
+++ year 1 sem 1 OOP/lab 4 - cycles/Exercise6.cpp
@@ -12,12 +12,6 @@ int main()
     cout << "Enter value for degrees: ";
     cin >> degrees;
 
-    // if (degrees < 45 || degrees > 60)
-    // {
-    //     cout << "The value for degrees should be between 45 and 60 (included). Enter other value for degrees: ";
-    //     cin >> degrees;
-    // }
-
     do
     {
         cout << "Incorrect value, degrees should be between 45 and 60 (included). Try again: ";
