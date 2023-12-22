7c9cd4f4d6cf9a415145d31ce257f4b09b496ecd
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-12-22 02:37:59 +0200

    changes in main

--- year 1 sem 1 OOP/Course Project Assignment/Person.cpp
+++ year 1 sem 1 OOP/Course Project Assignment/Person.cpp
@@ -79,6 +79,23 @@ void HourlyEmployee::displayEmployee()
 
 int main () 
 {
+    // TODO: do while doesn't work - fix it
+    int n;
+    int arr[n];
+
+    do
+    {
+        cout << "Enter number for employees(between 1 and 50): ";
+        cin >> n;
+    } while (n >= 0 && n <= 50);
+
+    // TODO: for loop from i = 0 until i < arr[n].length() 
+    // to print info for all employees
+
+    // TODO: for loop from i = 0 until i < arr[n].length()
+    // print names and monthly payment for all employees
+
+    // TODO: print avg monthly payment with hourly wages
 
     return 0;
 };
