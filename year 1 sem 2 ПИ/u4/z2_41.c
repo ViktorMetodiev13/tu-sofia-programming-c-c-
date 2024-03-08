63a11d2db7e0a588a04e5edd63925880fa946af0
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-03-08 18:16:37 +0200

    Exercise 1 from if else theme solution

---
+++ year 1 sem 2 ПИ/u4/z2_41.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter value for n: ");
+    scanf("%d", &n);
+
+    if (n % 5 == 0) 
+    {
+        printf("The number is dividable by 5 remainder.");
+    } 
+    else
+    {
+        
+    }
+    
+    return 0;
+}
